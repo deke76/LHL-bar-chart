6bea22cba7e561bc1a48b49ec8db4d907b0a8e0f
Darcy <darcy.lauder@shaw.ca>
2022-01-11 15:12:45 -0700

    Update prior to recoding
    Update prior to recoding

--- bar-charting.js
+++ bar-charting.js
@@ -1,15 +1,80 @@
+const chartOptions = {
+  chartWidth: 8,
+  chartHeight: 5,
+  chartScale: 1,
+  dimUnits: 'in',
+  barColor: 'black',
+  labelColor: 'black',
+  titleColor: 'black', 
+  titleSize: 20,
+  titleFont: 'arial',
+  axisFont: 'arial',
+  barSpacing: 0,
+  titleName: 'Name',
+  xAxisName: 'X Axis',
+  yAxisName: 'Y Axis',
+}
+
+function unitConversion( value, unitType ) {
+  return value * ( ( unitType === 'inches' ) ? 96 :    
+  ( unitType === 'millimeters' ) ? 3.78 : 37.8 )
+}
+
+function setupChartArea ( pageElement ) {
+  /* Set the area to a grid display using CSS grid:
+  Title   Title   
+  Y-Label Chart   
+  blank   X-Label*/
+  
+  pageElement.css( "display", "grid" );
+  pageElement.css( "grid-template-columns", "10% 90%" );
+  pageElement.css( "grid-template-rows", "20% 70% 10%");
+  pageElement.css( "grid-template-areas", '"title title" "y-label chart" ". x-label"');
+  pageElement.css( "width", "95%" );
+  pageElement.css( "height", "95%" );
+  pageElement.css( "padding", "10px" );
+  // pageElement.css( "background-color", "yellow" );
+  pageElement.append('<div id="title"></div>');
+  $('#title').css( 'grid-area', 'title' );
+  $('#title').css( 'place-self', 'center' );
+  $('#title').css( 'padding: 10px' );
+  pageElement.append('<div id="y-label"></div>');
+  $('#y-label').css( 'grid-area', 'y-label' );
+  $('#y-label').css( 'place-self', 'center' );
+  pageElement.append('<div id="chart"></div>');
+  $('#chart').css( 'grid-area', 'chart' );
+  $('#chart').css( 'place-self', 'end center' );
+  pageElement.append('<div id="x-label"></div>');
+  $('#x-label').css('grid-area', 'x-label' );
+  $('#x-label').css('place-self', 'center' );
+}
+
+function chartLabels ( chartName, ytitle, xtitle ) {
+  /* Write the chart labels into the charting area */
+  $('#title').text( chartName );
+  $('#y-label').css( 'writing-mode', 'vertical-lr' );
+  $('#y-label').css( 'text-orientation', 'upright' );
+  $('#y-label').text( ytitle );
+  $('#x-label').text( xtitle );
+}
+
 function drawAxis( location, units, x, y ) {
   
-  x += units;
-  y += units;
+  x += 'px';
+  y += 'px';
 
+  console.log( "x = " + x, "y = ", y );
   location.css( "width", x );
   location.css( "border-bottom-style", "solid" );
-  location.css( "width", y );
+  location.css( "height", y );
   location.css( "border-left-style", "solid" );
 }
 
-function drawBarChart( data, options, element ) {
-
-  drawAxis( element, '', options.width, options.height);
+function drawBarChart ( data, chartOptions, element ) {
+  //Setup the chart area to grid display for labels, apply labels, X/Y Axis
+  setupChartArea( element );
+  chartLabels( chartOptions.titleName, chartOptions.yAxisName, chartOptions.xAxisName );
+  drawAxis( $( '#chart' ), chartOptions.dimUnits, chartOptions.chartWidth, chartOptions.chartHeight);
 }
+
+drawBarChart( data, chartOptions, element);
--- main.js
+++ main.js
@@ -1,18 +1,15 @@
 let chartNumbers = [];
-let chartOptions = {
-  width: '100px',
-  height: '100px',
-};
-const chartSpace = $('#chart');
+const chartSpace = $('#chart-space');
 
 $(document).ready( () => {
 
   $('#left').on( 'click', () => {
-    $('.input-form').toggleClass("showform");
+    $('.input-form').toggleClass( "showform" );
+    $('#third-row-dl').toggleClass( "showform" );
   });
 
   $('#right').on( 'click', () => {
-    $('.options-form').toggleClass("showform");
+    $('.options-form').toggleClass( "showform");
   });
 
   $('#add-data').on( 'click', () => {
@@ -23,16 +20,39 @@ $(document).ready( () => {
       $('#number').val("")
       $('#chart-values').text( `${chartNumbers.length} data point(s) to graph.` );
       if (chartNumbers.length > 0 ) {
-        $('#graph-button').show();
+        if( $('#graph-button').css( "display" ) === 'none' ) {
+          $('#graph-button').toggleClass("showform");
+        };
       }
       // console.log(chartNumbers)
     }
   })
 
-  $('#graph-button').on( 'click', event => {
-    // console.log("update");
-    event.currentTarget.innerHTML ="Update";
+  $('#graph-button').on('click', () => {
+    $('.input-form').toggleClass( "showform" );
+    $('#third-row-dl').toggleClass( "showform" );
+    $('#graph-button').toggleClass( "showform" );
+    $('#chart-space').css( "display", "flex" );
+
+    chartOptions.dimUnits = $('#units').val();
+    chartOptions.chartWidth = unitConversion( $('#width').val(), chartOptions.dimUnits );
+    chartOptions.chartHeight = unitConversion( $('#height').val(), chartOptions.dimUnits);
+    chartOptions.chartScale = unitConversion( $('#y-scale').val(), chartOptions.dimUnits);
+    chartOptions.barColor = $('#bar-colors').val();
+    chartOptions.labelColor = $('#labels-colors').val();
+    chartOptions.titleColor = $('#title-colors').val();
+    chartOptions.titleSize = $('#chart-title-size').val();
+    chartOptions.titleFont = $('#chart-title-font').val();
+    chartOptions.axisFont = $('#axis-title-font').val();
+    chartOptions.barSpacing = $('#bar-spacing').val();
+    chartOptions.titleName = $('#chart-title').val();
+    chartOptions.xAxisName = $('#x-axis-labels').val();
+    chartOptions.yAxisName = $('#y-axis-labels').val();
+    console.log( chartOptions );
+
     drawBarChart( chartNumbers, chartOptions, chartSpace )
   })
 
 })
+
+console.log()
